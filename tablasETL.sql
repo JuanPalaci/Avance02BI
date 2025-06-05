06cf9dca970cad4cd70de49801e91cfccec404aa
iamcarlogg <00052220@uca.edu.sv>
2025-06-05 16:40:44 -0600

    Subiendo ETL con transformaciones

---
+++ tablasETL.sql
@@ -0,0 +1,11 @@
+CREATE DATABASE FarmaciaDW
+USE FarmaciaDW
+CREATE TABLE DimFechaFarmacia;
+
+CREATE TABLE InventarioFarmacia;
+
+CREATE TABLE ProductoFarmacia;
+
+CREATE TABLE SucursalFarmacia;
+
+CREATE TABLE VentasFarmacia;
